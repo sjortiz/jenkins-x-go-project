ca436b0ff2fa603e9695eb7469cd415e04a4e643
ShailynOrtiz <sortiz@intellisys.com.do>
2019-07-26 13:48:01 -0400

    Removing the url string

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello from:  "+title+ "at" + r.URL.String() + "\n")
+	fmt.Fprintf(w, "Hello from:  "+title+ "\n")
 }
 
 func main() {
